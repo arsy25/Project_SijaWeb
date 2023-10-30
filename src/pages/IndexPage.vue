456a7fcce64b8a56da1f6b24e6cf73e6df99bd44
arsy25 <ilhamalparsy01@gmail.com>
2023-10-30 21:33:45 +0700

    section home, lottie, navbar fix

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,11 @@
 <template>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+    rel="stylesheet"
+  />
+
   <q-layout view="hHh lpR fFf" class="scroll">
-    <q-header class="row navbar flex flex-center">
+    <q-header class="row navbar flex flex-center" id="navbar">
       <div class="col-1"></div>
       <div class="col-4 text-weight-bold text-h5">LOGO</div>
 
@@ -61,15 +66,20 @@
       <div class="col-2"></div>
     </q-header>
 
-    <section class="flex-section">
+    <section class="flex-section1">
       <q-card
         class="q-pa-md q-ml-xl borderhome"
-        style="width: 45%; height: 30%; margin-left: 15%"
+        style="width: 90%; height: 62%"
         flat
-        >abc
+      >
+        <div class="title">Sistem Informasi Jaringan dan Aplikasi</div>
       </q-card>
-      <div>a</div>
+      <div class="overlay-content q-mr-xl">
+        <lottie style="width: 590px" :options="defaultOptions" />
+      </div>
     </section>
+    <div class="tombolkebawah">abcv</div>
+    <div class="tombolkebawah">abcv</div>
 
     <section class="flex-section"></section>
     <section class="flex-section">Section 3</section>
@@ -77,12 +87,36 @@
 </template>
 
 <script>
+import Lottie from "./../components/lottie.vue";
+import * as animationData from "assets/coding.json";
 export default {
   name: "IndexPage",
+  components: {
+    Lottie,
+  },
+  data() {
+    return {
+      defaultOptions: { animationData: animationData.default },
+      animationSpeed: 2,
+    };
+  },
 };
+
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY >= 50) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+});
 </script>
 
 <style>
+.q-card {
+  position: relative;
+  z-index: 1;
+}
 .btn {
   border-radius: 20px;
 }
@@ -94,19 +128,31 @@ export default {
 }
 .navbar {
   background: transparent;
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-family: "Poppins", sans-serif;
   color: black;
   height: 50px;
+}
+.navbar-scrolled {
+  background: #e8e8eb;
+  transition: background-color 0.9s ease-out;
 }
 
 .flex-section {
   display: flex;
   align-items: center;
-  justify-content: left;
+  justify-content: center;
   height: 100vh;
   background-color: #f1f1f2; /* Ganti dengan warna latar belakang yang Anda inginkan */
-  text-align: left;
+  text-align: center;
   margin: 0;
+}
+.flex-section1 {
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  height: 95vh;
+  background-color: #f1f1f2; /* Ganti dengan warna latar belakang yang Anda inginkan */
+  margin-left: -10%;
 }
 
 .flex-section:nth-child(2) {
@@ -118,8 +164,30 @@ export default {
 }
 
 .borderhome {
+  position: relative;
   background: #7081f5;
-  border-radius: 30px;
+  border-radius: 60px;
+  margin-left: 15%;
+}
+.title {
+  font-family: Poppins;
+  font-size: 35px;
+  padding: 2%;
+  font-weight: 900;
+}
+.overlay-content {
+  position: relative;
+  z-index: 2; /* Atur z-index overlay lebih tinggi daripada q-card */
+  margin-left: -10%; /* Sesuaikan nilai negatif untuk menimpa bagian kiri */
+  margin-top: 10%;
+  margin-bottom: 10%;
+}
+.tombolkebawah {
+  background: #f1f1f2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 }
 
 * {
