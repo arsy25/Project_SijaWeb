5b5a689baaf2688548ddec66aeb1d64d39ef3205
arsy25 <ilhamalparsy01@gmail.com>
2023-10-31 14:23:32 +0700

    HOME SECTION COMPLETED

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,13 +3,17 @@
     href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
     rel="stylesheet"
   />
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
+  />
 
-  <q-layout view="hHh lpR fFf" class="scroll">
+  <q-layout view="hHh lpR fFf" class="bgall">
     <q-header class="row navbar flex flex-center" id="navbar">
       <div class="col-1"></div>
       <div class="col-4 text-weight-bold text-h5">LOGO</div>
 
-      <div class="col-1 q-mr-md">
+      <div class="col-1 q-mr-sm">
         <q-btn
           rounded
           flat
@@ -20,7 +24,7 @@
         ></q-btn>
       </div>
 
-      <div class="col-1 q-mr-md">
+      <div class="col-1 q-mr-xl">
         <q-btn
           rounded
           flat
@@ -31,7 +35,18 @@
         ></q-btn>
       </div>
 
-      <div class="col-1 q-mr-md">
+      <div class="col-1 q-mr-xl">
+        <q-btn
+          rounded
+          flat
+          style="width: 129px"
+          class="btn"
+          label="Documentation"
+          href="#"
+        ></q-btn>
+      </div>
+
+      <div class="col-1 q-mr-md q-ml-md">
         <q-btn
           rounded
           flat
@@ -42,7 +57,7 @@
         ></q-btn>
       </div>
 
-      <div class="col-1 q-mr-xl">
+      <!-- <div class="col-1 q-mr-xl q-ml-md">
         <q-btn
           rounded
           flat
@@ -51,18 +66,8 @@
           label="Contact"
           href="#"
         ></q-btn>
-      </div>
+      </div> -->
 
-      <div class="col-1 q-mr-sm">
-        <q-btn
-          rounded
-          flat
-          style="width: 123px"
-          class="btn"
-          label="Documentation"
-          href="#"
-        ></q-btn>
-      </div>
       <div class="col-2"></div>
     </q-header>
 
@@ -73,16 +78,32 @@
         flat
       >
         <div class="title">Sistem Informasi Jaringan dan Aplikasi</div>
+        <p class="capt">
+          SIJA merupakan singkatan dari Sistem Informasi, Jaringan, dan
+          Aplikasi. Dimana kompetensi keahlian ini khusus ditempuh selama 4
+          tahun. Mempersiapkan tenaga kerja tingkat menengah yang terampil dalam
+          bidang teknologi informasi Komputer Jaringan Dasar dan Sistem
+          Komputer.
+        </p>
+        <div class="demovid q-gutter-xl">
+          <q-btn class="btdemo" flat>Demo Video</q-btn>
+          <q-btn class="btdemo" flat>Explore</q-btn>
+        </div>
       </q-card>
       <div class="overlay-content q-mr-xl">
         <lottie style="width: 590px" :options="defaultOptions" />
       </div>
     </section>
-    <div class="tombolkebawah">abcv</div>
-    <div class="tombolkebawah">abcv</div>
+    <div class="tombolkebawah">
+      <q-btn class="btreadmore" flat rounded
+        >read more
+        <div class="tombolkebawah"><i class="bi bi-arrow-down-short"></i></div>
+      </q-btn>
+    </div>
 
-    <section class="flex-section"></section>
-    <section class="flex-section">Section 3</section>
+    <section class="flex-section">Learn</section>
+    <section class="flex-section">Documentation</section>
+    <section class="flex-section">About</section>
   </q-layout>
 </template>
 
@@ -113,6 +134,32 @@ window.addEventListener("scroll", function () {
 </script>
 
 <style>
+.bgall {
+  background: linear-gradient(
+    321deg,
+    #f1f1f2,
+    #a2abe9,
+    #f1f1f2,
+    #f1f1f2,
+    #f1f1f2
+  );
+  background-size: 400% 400%;
+
+  animation: AnimationName 11s ease infinite;
+}
+
+@keyframes AnimationName {
+  0% {
+    background-position: 32% 0%;
+  }
+  50% {
+    background-position: 69% 100%;
+  }
+  100% {
+    background-position: 32% 0%;
+  }
+}
+
 .q-card {
   position: relative;
   z-index: 1;
@@ -123,6 +170,7 @@ window.addEventListener("scroll", function () {
 .btn:hover {
   background-color: #7081f5;
   font-weight: 600;
+  height: 10px;
   transition: background-color 0.9s ease-in-out, color 0.9s ease-in-out,
     font-weight 0.3s ease-in-out;
 }
@@ -130,10 +178,10 @@ window.addEventListener("scroll", function () {
   background: transparent;
   font-family: "Poppins", sans-serif;
   color: black;
-  height: 50px;
+  height: 38px;
 }
 .navbar-scrolled {
-  background: #e8e8eb;
+  background: #f1f1f2;
   transition: background-color 0.9s ease-out;
 }
 
@@ -142,7 +190,6 @@ window.addEventListener("scroll", function () {
   align-items: center;
   justify-content: center;
   height: 100vh;
-  background-color: #f1f1f2; /* Ganti dengan warna latar belakang yang Anda inginkan */
   text-align: center;
   margin: 0;
 }
@@ -151,16 +198,8 @@ window.addEventListener("scroll", function () {
   align-items: center;
   justify-content: left;
   height: 95vh;
-  background-color: #f1f1f2; /* Ganti dengan warna latar belakang yang Anda inginkan */
+  /* background-color: #f1f1f2; Ganti dengan warna latar belakang yang Anda inginkan */
   margin-left: -10%;
-}
-
-.flex-section:nth-child(2) {
-  background-color: #f1f1f2;
-}
-
-.flex-section:nth-child(3) {
-  background-color: #f1f1f2;
 }
 
 .borderhome {
@@ -175,6 +214,32 @@ window.addEventListener("scroll", function () {
   padding: 2%;
   font-weight: 900;
 }
+.capt {
+  font-family: Poppins;
+  padding: 2%;
+  margin-top: -2%;
+}
+.demovid {
+  padding: 2%;
+}
+.btreadmore {
+  background: transparent;
+  height: 50px;
+}
+.btdemo {
+  background: #e8e8eb;
+  border: solid 1px;
+  border-color: #000000;
+  border-radius: 30px;
+  font-family: Poppins;
+  width: 30%;
+}
+
+.btdemo:hover {
+  background: #000000;
+  color: #e8e8eb;
+}
+
 .overlay-content {
   position: relative;
   z-index: 2; /* Atur z-index overlay lebih tinggi daripada q-card */
@@ -183,11 +248,14 @@ window.addEventListener("scroll", function () {
   margin-bottom: 10%;
 }
 .tombolkebawah {
-  background: #f1f1f2;
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-top: -2%;
   width: 100%;
+}
+.iconread {
+  margin-top: -1%;
 }
 
 * {
